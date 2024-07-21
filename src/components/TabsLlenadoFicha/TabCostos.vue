581fdb943d675bc6b97dce3a9f40a8d68b3547c7
MarqueG <marquezgstudy@gmail.com>
2024-07-21 19:31:55 -0400

    Corregido la parte de llenado mejorada la parte de creacion de nuevo usuarios

--- src/components/TabsLlenadoFicha/TabCostos.vue
+++ src/components/TabsLlenadoFicha/TabCostos.vue
@@ -1,6 +1,13 @@
 <template>
     <h6>Estimación de Costo</h6>
     <CrearCosto/>
+
+    <div class="row center">
+    <div class="divider"></div>
+    <button class="btn">
+      <span> <i class="material-icons"> save </i>Guardar </span>
+    </button>
+  </div>
 </template>
 
 <script>
@@ -12,3 +19,20 @@
         }
     }
 </script>
+
+<style scoped>
+.btn {
+  margin: 20px 10px 0 0;
+  background-color: rgb(52, 52, 97) !important;
+}
+.btn:hover {
+  background-color: rgb(98, 98, 155) !important;
+}
+.btn span {
+  display: flex;
+  gap: 5px;
+}
+.btn i {
+  font-size: 24px;
+}
+</style>
